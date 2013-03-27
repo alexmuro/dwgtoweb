f1d751f7789e4aecf6fca0b7a4802318e14a1e5e
a <alex@persephone.(none)>
2013-03-27 14:40:07 -0400

    corrected dir

--- upload/totxt.php
+++ upload/totxt.php
@@ -2,6 +2,6 @@
 $inputfile = $_POST['upload'];
 echo $inputfile;
 
-$cmd = "../text2db/AutoCADConverter 'uploads/".$inputfile."' 'txts/".$inputfile.".txt'";
+$cmd = "../text2db/AutoCADConverter '../text2db/uploads/".$inputfile."' '../text2db/txts/".$inputfile.".txt'";
 echo $cmd;
 echo exec($cmd);
