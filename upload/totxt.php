758dabfd6baf0b0c6f6e020e4cca10d32bba69a9
a <alex@persephone.(none)>
2013-03-27 14:38:33 -0400

    add quotes to file names for autocadconverter exec call

--- upload/totxt.php
+++ upload/totxt.php
@@ -2,6 +2,6 @@
 $inputfile = $_POST['upload'];
 echo $inputfile;
 
-$cmd = "../text2db/AutoCADConverter uploads/".$inputfile." txts/".$inputfile.".txt";
+$cmd = "../text2db/AutoCADConverter 'uploads/".$inputfile."' 'txts/".$inputfile.".txt'";
 echo $cmd;
 echo exec($cmd);
