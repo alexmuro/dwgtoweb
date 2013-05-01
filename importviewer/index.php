c2b883c41687541eb59db4ef0ead0dba3daead93
Alex Muro <am3081@gmail.com>
2013-05-01 16:23:12 -0400

    project clean up

--- importviewer/index.php
+++ importviewer/index.php
@@ -1,30 +1,26 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <title>Imported DWG File Viewer</title>
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="resources/css/ext-all.css"/>
+        <link rel="stylesheet" type="text/css" href="resources/css/blue-table-style.css"/>
+        <link rel="stylesheet" type="text/css" href="resources/css/style.css"/>        
+        <!-- Javascript -->
+        <script src="resources/js/openlayers.min.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script type="text/javascript" src="resources/js/ext-base.js"></script>
         <script type="text/javascript" src="resources/js/ext-all.js"></script>
-        <script src="openlayers/lib/OpenLayers.js"></script>
         <script type="text/javascript" src="resources/js/GeoExt.js"></script>
         <script type="text/javascript" src="resources/js/jquery.tablesorter.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="resources/css/ext-all.css"/>
-        <link rel="stylesheet" type="text/css" href="resources/css/blue-table-style.css"/>
         
-        <link rel="stylesheet" type="text/css" href="style.css"/>
 
-         
-
-        
         <script type="text/javascript">
         //Globals
         var mapPanel, tree,map;
         </script>
         <!-- Viewer Files -->
         <script type="text/javascript" src='../js/getJson.js'></script>
-        <script type="text/javascript" src='resources/js/behavior.js'></script>
-        
-        <!-- Uploader Files -->
-
+        <script type="text/javascript" src='partials/behavior.js'></script>
 
         <!-- Main js file -->
         <script type="text/javascript" src="app.js"></script>
